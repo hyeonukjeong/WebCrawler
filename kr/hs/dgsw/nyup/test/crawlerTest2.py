570d8c5ff4cbcf7ec8f2683299dd2fd6173b5fb3
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-01-19 13:38:08 +0900

    20180119

--- kr/hs/dgsw/nyup/test/crawlerTest2.py
+++ kr/hs/dgsw/nyup/test/crawlerTest2.py
@@ -5,5 +5,5 @@ soup = BeautifulSoup(html, 'lxml')
 titles = soup.find_all('a', 'title')
 
 for title in titles:
-    w = '제목 : {0:20s} 링크 : comic.naver.com{1:20s}\n'.format(title['title'], title['href'])
+    w = '제목 : {0:20s}\n링크 : comic.naver.com{1:20s}\n'.format(title['title'], title['href'])
     print(w)
